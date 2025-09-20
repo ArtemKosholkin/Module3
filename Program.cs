1b95d932ef5ac96d16af0aad70b81cd298608478
kaa <kaa@sdexport.ru>
2025-09-20 22:49:52 +0700

    Урок 3.2.

--- Program.cs
+++ Program.cs
@@ -10,10 +10,15 @@ namespace Module3
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Artem";
-
+            const string myName = "Artem";
+            
             Console.WriteLine(myName);
+            Console.WriteLine("\t Привет, Мир!");
+            Console.WriteLine("\t Меня зовут \n Артем");
+            Console.WriteLine('\u0023'); //вывод символа # согласно ASCII
+            Console.WriteLine('\x23'); //можно и так, только непонятно, почему?
+
+            Console.WriteLine("\n Для продолжения нажмите любую кнопку");
             Console.ReadKey();
         }
     }
