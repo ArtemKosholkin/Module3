bcb13b1d521735390e8fc387d9bb03a06e1315e4
kaa <kaa@sdexport.ru>
2025-09-22 18:04:12 +0700

    Урок 3.3. Окончание.

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@ namespace Module3
     {
         static void Main(string[] args)
         {
-            /*
-            const string myName = "Artem";
-            
-            Console.WriteLine(myName);
-            Console.WriteLine("\t Привет, Мир!");
-            Console.WriteLine("\t Меня зовут \n Артем");
-            Console.WriteLine('\u0023'); //вывод символа # согласно ASCII
-            Console.WriteLine('\x23'); //можно и так, только непонятно, почему?
-            */
+            //const string myName = "Artem";
+            //Console.WriteLine(myName);
+            //Console.WriteLine("\t Привет, Мир!");
+            //Console.WriteLine("\t Меня зовут \n Артем");
+            //Console.WriteLine('\u0023'); //вывод символа # согласно ASCII
+            //Console.WriteLine('\x23'); //можно и так, только непонятно, почему?
+
+            //Console.WriteLine("IntMinValue = {0}", int.MinValue);
+            //Console.WriteLine("ByteMaxValue = {0}", byte.MaxValue);
+
             string myName = "Artem";
             byte myAge = 35;
             bool myPet = true;
